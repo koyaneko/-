6238c1f79c0552ad0412430a76344f60faf30cd4
kudo <108576161+koyaneko @users.noreply.github.com>
2024-12-13 20:06:32 +0900

    敗北時のコメントをランダムで出力されるよう変更

--- game/janken.sh
+++ game/janken.sh
@@ -23,7 +23,32 @@ do
   echo "コンピュータの手が正しく処理されませんでした、ゲームを終了します"
   exit 1;
  fi
+HAIBOKU=$(($RANDOM % 10)) 
 
+ if [ $HAIBOKU = 0 ]; then
+  you_loose_dec="残念、あなたの負けです。外出は控えましょう。"
+ elif [ $HAIBOKU = 1 ]; then
+  you_loose_dec="残念、私の勝ちです。1時間勉強しましょう。約束ですよ？"
+ elif [ $HAIBOKU = 2 ]; then
+  you_loose_dec="残念、私の勝ちです。私に勝とうなんて100年早いです。"
+ elif [ $HAIBOKU = 3 ]; then
+  you_loose_dec="残念、あなたの負けです。今日は甘いものは控えましょう。"
+ elif [ $HAIBOKU = 4 ]; then
+  you_loose_dec="残念だったな。怖いか？この力が・・・。"
+ elif [ $HAIBOKU = 5 ]; then
+  you_loose_dec="残念、あなたの負けです。今日の夕飯は白米禁止です。"
+ elif [ $HAIBOKU = 6 ]; then
+  you_loose_dec="残念、私の勝ちです。ゲームはログインだけなら許可します。"
+ elif [ $HAIBOKU = 7 ]; then
+  you_loose_dec="残念、あなたの負けです。今日は動画鑑賞禁止です。"
+ elif [ $HAIBOKU = 8 ]; then
+  you_loose_dec="残念。あなたの負けです。飲み会もほどほどに。"
+ elif [ $HAIBOKU = 9 ]; then
+  you_loose_dec="残念、私の勝ちです。またの挑戦をお待ちしております。"
+ else
+  echo "敗北時のコメントが正しく処理されませんでした、ゲームを終了します"
+  exit 1;
+ fi
  #ゲーム開始
  echo "グー・チョキ・パーのどれかを入力してください"
 
@@ -42,12 +67,15 @@ do
  fi
 done
 
+
+
+
  if [ $com_hand_dec = "グー" ]; then
   if [ $yourhand = "パー" ]; then
    echo "おめでとうございます！あなたの勝ちです！"
    exit 0;
   else
-  echo "残念、あなたの負けです。外出は控えましょう。"
+  echo $you_loose_dec
   exit 0;
   fi
  fi
@@ -57,7 +85,7 @@ done
    echo "おめでとうございます！あなたの勝ちです！"
    exit 0;
   else
-   echo "残念、あなたの負けです。壺を買ったりしないようにしましょう。"
+   echo $you_loose_dec
    exit 0;
   fi
  fi
@@ -67,7 +95,7 @@ done
    echo "おめでとうございます！あなたの勝ちです！"
    exit 0;
   else
-   echo "残念、あなたの負けです。転ばないように注意しましょう。"
+   echo $you_loose_dec
    exit 0;
   fi
  fi
